0e2fb8a22c5646fbbebf62c405f7ced821e4909d
Abu Talha <abutalha141999@gmail.com>
2022-10-11 21:28:01 +0600

    recharts created

--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-
+import React, { useContext } from "react";
+import { Line, LineChart } from "recharts";
+import { GetHeaderData } from "../Layout/Main";
 const Statistics = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const datum = useContext(GetHeaderData)
+    const data = JSON.stringify(datum.data)
+    console.log(data)
+  return (
+    <div className="w-1/2 mx-auto mt-12">
+      <LineChart width={600} height={300} data={data}>
+        <Line type="monotone" dataKey="name" stroke="#8884d8" />
+      </LineChart>
+    </div>
+  );
 };
 
 export default Statistics;
---
+++ src/Components/ViewStatics.jsx
@@ -0,0 +1,5 @@
+
+const ViewStatics = () => {
+}
+
+export default ViewStatics;
