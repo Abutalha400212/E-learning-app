1f45338880b6c2e43aa2e7198b8b63ab684da67d
Abu Talha <abutalha141999@gmail.com>
2022-10-12 13:08:52 +0600

    eye toggle icon and its interection created

--- src/Components/QuizQuestion.jsx
+++ src/Components/QuizQuestion.jsx
@@ -4,27 +4,27 @@ import ShowQuestion from "./ShowQuestion";
 import { BackwardIcon } from "@heroicons/react/24/solid";
 const QuizQuestion = () => {
   const QuizData = useLoaderData();
-  const quiz = QuizData.data.questions
+  const quiz = QuizData.data.questions;
   return (
     <div>
-      <div className="w-11/12 mx-32 mt-12 md:flex justify-between">
+      <div className="md:w-11/12 w-11/12 mx-8  md:mx-32 mt-12 md:flex md:justify-between">
         <ul className="grid grid-cols-1 gap-y-3 list-decimal ">
           {quiz.map((data) => {
-            return <ShowQuestion
-                key={data.id}
-                data={data}
-              ></ShowQuestion>}
-            )};
+            return <ShowQuestion key={data.id} data={data}></ShowQuestion>;
+          })}
+          ;
         </ul>
-      <div className="w-1/12">
-       <h1>Correct Ans</h1>
-      </div>
+        <div className="md:w-1/12">
+          <h1>Correct Ans</h1>
+        </div>
       </div>
       <div className="bg-yellow-500 p-4 rounded absolute mb-0 mt-4 mx-2 flex">
         <BackwardIcon className="h-6 w-6 mr-2 text-blue-500" />{" "}
-        <button><Link to="/topics">Back To Another Quiz</Link> </button>
+        <button>
+          <Link to="/topics">Back To Another Quiz</Link>{" "}
+        </button>
       </div>
-      </div>
+    </div>
   );
 };
 
--- src/Components/ShowQuestion.jsx
+++ src/Components/ShowQuestion.jsx
@@ -1,50 +1,51 @@
+import {EyeIcon } from "@heroicons/react/24/solid";
 import { createContext, useState } from "react";
 import { toast } from "react-toastify";
 import Options from "./Options";
 export const CountContext = createContext([]);
-export const countAns = createContext([])
+export const countAns = createContext([]);
 const ShowQuestion = ({ data }) => {
+  const [toggle,setToggle] = useState(true)
   const ans = data.correctAnswer;
   const options = data.options;
   const QuestionName = data.question;
-  const [rs, setRs] = useState([]);
   let count = 0;
   const setAns = (props) => {
-    const value = props.target.innerText
-    console.log(value)
+    const value = props.target.innerText;
+    console.log(value);
     if (value === ans) {
-      count = count + 1
-      console.log(count)
+      count = count + 1;
       toast.success("Wow! Your Ans is Correct", { autoClose: 500 });
     } else {
-      setRs(ans);
       toast.warning("Woh NO! Wrong Answer", { autoClose: 500 });
     }
   };
   return (
     <CountContext.Provider value={setAns}>
       <countAns.Provider value={count}>
-      <div>
         <div>
-          <li>
-            <span className="text-md font-bold">No. Question:</span>
-            <br />
-            <span className="text-purple-900 text-lg font-serif">
-              {QuestionName.replace(/(<([^>]+)>)/ig,"")}
-            </span>
-            <div className="grid md:grid-cols-2 gap-2">
-              {options.map((options) => (
-                <Options options={options}></Options>
-              ))}
-            </div>
-          </li>
-        </div>
-        <div>
-            <h1>
-              <small> Right Ans is: {rs}</small>
+          <div>
+            <li>
+              <div className="flex justify-between">
+                <span className="text-md font-bold">No. Question:</span>
+                <EyeIcon onClick={()=> setToggle(!toggle) } className="h-6 w-6 text-blue-500 cursor-pointer"/>
+              </div>
+                <span className="text-purple-900 text-lg font-serif">
+                  {QuestionName.replace(/(<([^>]+)>)/gi, "")}
+                </span> 
+              <div className="grid md:grid-cols-2 gap-2">
+                {options.map((options) => (
+                  <Options options={options}></Options>
+                ))}
+              </div>
+            </li>
+          </div>
+          <div className={`${toggle ? 'hidden' : 'right' }`}>
+          <h1>
+              <small> Right Ans is: {ans}</small>
             </h1>
+          </div>
         </div>
-      </div>
       </countAns.Provider>
     </CountContext.Provider>
   );
